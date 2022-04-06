cf6181a0a25639c5e5c4be18ec2b6cd95bdff386
maddiekash <mmk86@pitt.edu>
2022-04-06 17:10:23 -0400

    Updated data

--- Trial/app.R
+++ Trial/app.R
@@ -12,18 +12,20 @@ library(ggplot2)
 library(tidyverse)
 HighSchool <- read.csv("HighSchool2.csv")
 Bachelor <- read.csv("Bachelor1.csv")
+HSerror <- read.csv("HighSchoolError.csv")
+Berror <- read.csv("BachelorError.csv")
 
 # Define UI for application that draws barplot
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Educational Attainment By State"),
+    titlePanel("Educational Attainment By State and Race/Ethnicity"),
 
     # sidebar with dropdown
   
     sidebarLayout(
         sidebarPanel(
-          radioButtons("level", "Choose Education Level to View",
+          radioButtons("level", "Choose Education Level to View:",
                        c("Highschool and Over", "Bachelor's and Over")),
           selectInput("state", "Choose A State:", 
                       choices=colnames(HighSchool[2:51])),
@@ -41,23 +43,36 @@ ui <- fluidPage(
 server <- function(input, output, session) {
     observe({
       updateSelectInput(session, "state", "Choose A State:", 
-                  choices=colnames(HighSchool[2:51]))
+                  choices=colnames(HighSchool[2:53]))
     })
   
   
-    hs <- reactive({
-      if(input$level=="Highschool and Over"){HighSchool[,c("Race",input$state)]
+    edu <- reactive({
+      if(input$level=="Highschool and Over"){
+        df=data.frame(Race=HighSchool[,"Race"], 
+                      state=HighSchool[,input$state],
+                      error=HSerror[,input$state])
+        return(df)
         }
-      else(Bachelor[,c("Race",input$state)])   
+      else({
+        df=data.frame(Race=Bachelor[,"Race"], 
+                         state=Bachelor[,input$state],
+                         error=Berror[,input$state])
+        return(df)
+        })   
     })
-    
+  
+
     output$StatePlot <- renderPlot({
         # Render a barplot
-        ggplot(hs(), aes(x=Race,y=hs()[,input$state],fill=Race)) +
+        ggplot(edu(), aes(x=Race,y=state,fill=Race)) +
             geom_col() +
             ggtitle("Educational Attainment Level By Race") +
             xlab("Race") +
-            ylab("Percent Attainment") 
+            ylab("Percent of Population") +
+            geom_errorbar(aes(x=Race,
+                          ymin=state-error,
+                          ymax=state+error))
     })
 }
 
