5a010430365e0857f34127709ea4ee0cc896baa5
alextistics <97633202+alextistics@users.noreply.github.com>
2022-04-15 13:14:02 -0400

    Fixed Literally Everthing

--- Trial/app.R
+++ Trial/app.R
@@ -14,14 +14,14 @@ library(ggiraph)
 library(sf)
 library(crayon)
 
-HighSchool <- as.data.frame(read_csv("HighSchool2.csv"))
-Bachelor <- as.data.frame(read_csv("Bachelor1.csv"))
-HSerror <- as.data.frame(read_csv("HighSchoolError.csv"))
-Berror <- as.data.frame(read_csv("BachelorError.csv"))
+HighSchool <- as.data.frame(read_csv("Trial/HighSchool2.csv"))
+Bachelor <- as.data.frame(read_csv("Trial/Bachelor1.csv"))
+HSerror <- as.data.frame(read_csv("Trial/HighSchoolError.csv"))
+Berror <- as.data.frame(read_csv("Trial/BachelorError.csv"))
 
-bach_adj_map <- read_csv("bach_adj.csv")
-hs_adj_map <- read_csv("hs_adj.csv")
-us <- st_read("states_map.shp")
+bach_adj_map <- read_csv("Trial/bach_adj.csv")
+hs_adj_map <- read_csv("Trial/hs_adj.csv")
+us <- st_read("Trial/states_map.shp")
 
 
 # Define UI for application that draws barplot
@@ -125,23 +125,23 @@ server <- function(input, output, session) {
     
     plot_df <- reactive({
       state_attain <- edu() %>% 
-        select("Race", state) %>% 
-        rename(paste(input$state) = state)
+        select(Race, state) %>% 
+        rename(state = state)
       state_error <- edu() %>% 
-        select("Race", error) %>% 
-        rename(paste(input$state) = error)
+        select(Race, error) %>% 
+        rename(state = error)
       total_attain <- total() %>% 
-        select("Race", state) %>%
-        rename("US Average" = state)
+        select(Race, state) %>%
+        rename('US Average' = state)
       total_error <- total() %>% 
-        select("Race", error) %>% 
-        rename("US Average" = error)
+        select(Race, error) %>% 
+        rename('US Average' = error)
       full_attain <- full_join(state_attain, total_attain, by = "Race") %>% 
-        pivot_longer(c(paste(input$state), "US Average"), 
+        pivot_longer(c('state', "US Average"), 
                      names_to = "state",
                      values_to = "attainment")
       full_error <- full_join(state_error, total_error, by = "Race") %>%
-        pivot_longer(c(paste(input$state), "US Average"), 
+        pivot_longer(c('state', 'US Average'), 
                      names_to = "state",
                      values_to = "error")
       full_df <- full_join(full_attain, full_error, by = c("Race", "state"))
@@ -153,29 +153,44 @@ server <- function(input, output, session) {
     output$MapPlot <- renderGirafe({
     
       map_interactive <- ggplot(edu_map()) + 
-        geom_sf_interactive(aes(fill = !!input$race, tooltip = paste(state_name, sprintf("%+g",!!input$race), sep=": "), data_id = state_name)) + 
-        theme_void() + labs(fill = "Margin") + ggtitle(paste("State-by-State Comparison to U.S. Average: \n",raceSel(),measure()))
+        geom_sf_interactive(aes(fill = !!input$race, 
+                                tooltip = paste(state_name, 
+                                                sprintf("%+g",!!input$race), 
+                                                sep=": "), 
+                                data_id = state_name)) + 
+        theme_void() + 
+        labs(fill = "Margin") + 
+        ggtitle(paste("State-by-State Comparison to U.S. Average: \n",
+                      raceSel(),
+                      measure())) +
+        scale_fill_gradient2(low = "#CB4335", 
+                             mid = "white", 
+                             high = "#2E86C1", 
+                             na.value = "black")
       
       x <- girafe(ggobj = map_interactive, 
                   options = list(opts_selection(type = "single", 
                                                 only_shiny = FALSE,
-                                                css = "fill:yellow;stroke:gray;")))
+                                                css = "fill:yellow;stroke:grey;")))
       
     })
     output$StatePlot <- renderPlot({
       # Render a barplot
       ggplot(plot_df(), aes(x=Race,y=attainment,fill=state)) +
-        geom_col() + 
+        geom_col(position = 'dodge') + 
         ggtitle(paste(measure(),"by Race in",stateSelection())) +
-        xlab("Race") +
-        ylab("Percent with Attainment") +
+        scale_fill_discrete(name = "Region", 
+                            labels = c(stateSelection(), "US Average")) + 
+        scale_fill_manual(values = c("#CB4335", "#2E86C1")) + 
+        labs(x = "Race", y = "Percent with Attainment", fill = "Region") +
         geom_errorbar(aes(x=Race,
                           ymin=attainment-error,
                           ymax=attainment+error, 
                           group = state), 
                       size = 0.5,
                       width = 0.6,
-                      position = position_dodge(0.9))
+                      position = position_dodge(0.9)) + 
+        theme_minimal()
     })
     observeEvent(stateClick(), {
       updateSelectInput(session, "state", 
